71c5b46840e9f76f0f6ff60134ab7197b772b76d
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-03-15 16:18:27 +0100

    Update project configuration and add new files with tailwind v4

---
+++ apps/web/app/app.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Bonjour le monde!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/web',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [
+    !process.env.VITEST && reactRouter(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    tailwindcss(),
+  ],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    outDir: '../../dist/apps/web',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
