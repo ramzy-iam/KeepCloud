754901e9ac5a3ecfaa21c1ffa4c1d69223b03296
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-04-19 21:25:07 +0100

    file tree v2

--- apps/web/app/components/layouts/sidebar/menus/folder.tsx
+++ apps/web/app/components/layouts/sidebar/menus/folder.tsx
@@ -18,7 +18,7 @@ const File = ({
   noIcon?: boolean;
 }) => {
   return (
-    <div className="pl-6">
+    <div className="w-[200px] pl-6">
       <SidebarMenuButton className="flex cursor-pointer items-center gap-3">
         {!noIcon && icon}
         {name}
@@ -32,31 +32,35 @@ const Folder = ({
   icon = <FolderIcon />,
   noIcon = false,
   children,
-  root = false,
 }: {
   name: string;
   icon?: React.ReactNode;
   children?: React.ReactNode;
   noIcon?: boolean;
-  root?: boolean;
 }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Collapsible open={open} onOpenChange={setOpen} className="h-full">
-      <SidebarMenuButton className="flex w-[217px] cursor-pointer items-center gap-3">
+    <Collapsible
+      open={open}
+      onOpenChange={setOpen}
+      className="h-full w-[200px]"
+    >
+      <SidebarMenuButton className="flex w-full cursor-pointer items-center gap-3">
         <CollapsibleTrigger asChild>
-          <div onClick={() => setOpen(!open)}>
+          <button onClick={() => setOpen((prev) => !prev)} tabIndex={0}>
             <ChevronRightIcon
               width={16}
               height={16}
               className={`transition-transform ${open ? 'rotate-90' : ''}`}
             />
-          </div>
+          </button>
         </CollapsibleTrigger>
-        <div className="flex items-center gap-3">
-          {!noIcon && icon}
-          {name}
+        <div className="flex items-center gap-3 truncate">
+          {!noIcon && <span>{icon}</span>}
+          <span className="truncate" title={name}>
+            {name}
+          </span>
         </div>
       </SidebarMenuButton>
       <CollapsibleContent className="-pl-3 relative before:absolute before:top-0 before:bottom-0 before:left-2 before:w-px before:bg-border">
@@ -73,6 +77,7 @@ export const FileTree = () => {
         <File name="File A1" />
       </Folder>
       <Folder name="Folder B" />
+      <Folder name="Folder Very long " />
       <Folder name="Folder C">
         <File name="File C2" />
         <Folder name="Folder C">
@@ -86,7 +91,23 @@ export const FileTree = () => {
                   <Folder name="Folder C">
                     <File name="File C2" />
                     <Folder name="Folder C">
-                      <File name="File C2" />
+                      <Folder name="Folder C">
+                        <File name="File C2" />
+                        <Folder name="Folder C">
+                          <File name="File C2" />
+                          <Folder name="Folder C">
+                            <File name="File C2" />
+                            <Folder name="Folder C">
+                              <Folder name="Folder C">
+                                <File name="File C2" />
+                                <Folder name="Folder C">
+                                  <File name="File C2" />
+                                </Folder>
+                              </Folder>
+                            </Folder>
+                          </Folder>
+                        </Folder>
+                      </Folder>
                     </Folder>
                   </Folder>
                 </Folder>
