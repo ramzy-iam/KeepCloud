7bf3280c9e5520b762d491a3020dfad2c79fa100
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-03 15:34:39 +0100

    Update FolderView component with className prop for custom styling

--- apps/web/app/routes/app/home/explorer.tsx
+++ apps/web/app/routes/app/home/explorer.tsx
@@ -4,7 +4,12 @@ import { files, folders } from '@keepcloud/commons/types';
 export default function ExplorerComponent() {
   return (
     <div className="flex flex-col gap-6">
-      <FolderView data={folders} title="Suggested Folders" fixedView="grid" />
+      <FolderView
+        data={folders}
+        title="Suggested Folders"
+        fixedView="grid"
+        className="mb-0"
+      />
       <FolderView data={files} title="Suggested Files" group={true} />
     </div>
   );
