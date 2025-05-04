295122a5f5fdf56baf22bfb8a0e0b7d241bb571a
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-05 00:05:14 +0100

    Add migration_lock.toml and subscriptionPlan.prisma, delete author.prisma and post.prisma, and create new models: sharedFile.prisma, user.prisma, and file.prisma

--- libs/core/db/src/lib/schema/migrations/20250504221625_create_nanoid/migration.sql
+++ libs/core/db/src/lib/schema/migrations/20250504221625_create_nanoid/migration.sql
@@ -1,33 +1,8 @@
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
--- Generate a random nanoid of specified size
-CREATE OR REPLACE FUNCTION nanoid(size int DEFAULT 30)
-RETURNS text AS $$
-DECLARE
-  id text := '';
-  i int := 0;
-  urlAlphabet char(64) := 'ModuleSymbhasOwnPr-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW';
-  bytes bytea := gen_random_bytes(size);
-  byte int;
-  pos int;
-BEGIN
-  WHILE i < size LOOP
-    byte := get_byte(bytes, i);
-    pos := (byte & 63) + 1;
-    id := id || substr(urlAlphabet, pos, 1);
-    i := i + 1;
-  END LOOP;
-  RETURN id;
-END
-$$ LANGUAGE PLPGSQL VOLATILE;
-
-
-
 -- Generate nanoid with timestamp hash
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE OR REPLACE FUNCTION nanoid_timestamp_aware(
+
+CREATE OR REPLACE FUNCTION nanoid(
   nano_size int DEFAULT 21,        -- Length of the random nanoid part
   hash_size int DEFAULT 9,         -- Length of the timestamp-hash part
   alphabet text DEFAULT 'ModuleSymbhasOwnPr-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW'
