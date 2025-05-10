1f17b44a792e93445e8b89c09cc19f559d33d76c
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-10 20:14:10 +0100

    Refactor exception classes to simplify constructor parameters

--- libs/commons/backend/src/lib/exceptions/base.exception.ts
+++ libs/commons/backend/src/lib/exceptions/base.exception.ts
@@ -2,34 +2,32 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class AppException extends HttpException {
   constructor(
-    public readonly code: string,
-    public readonly details: Array<{
-      code: string;
-      message: string;
-      field?: string;
-    }>,
-    public readonly statusCode: HttpStatus,
+    code: string,
+    message: string,
+    status: HttpStatus,
+    field?: string,
   ) {
     super(
       {
         code,
-        details,
+        details: [
+          {
+            code,
+            message,
+            ...(field ? { field } : {}),
+          },
+        ],
       },
-      statusCode,
+      status,
     );
   }
 
   static create(
     code: string,
     message: string,
-    errorCode: string,
     status: HttpStatus,
     field?: string,
   ): AppException {
-    return new AppException(
-      code,
-      [{ code: errorCode, message, field }],
-      status,
-    );
+    return new AppException(code, message, status, field);
   }
 }
