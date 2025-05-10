a9e336d0f8f4d0d869a65af695587f01d89e31a1
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-10 01:49:43 +0100

    chore:replace typeorm with prisma

--- libs/core/db/src/lib/repositories/file/file.repository.ts
+++ libs/core/db/src/lib/repositories/file/file.repository.ts
@@ -1,19 +1,21 @@
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { FileScope } from './file.scope';
 import { File } from '../../entities';
+import { BaseRepository } from '../base';
+import { PrismaService } from '../../prisma';
 
 @Injectable()
-export class FileRepository extends Repository<File> {
-  constructor(
-    @InjectRepository(File)
-    private readonly repository: Repository<File>
-  ) {
-    super(repository.target, repository.manager, repository.queryRunner);
-  }
-
-  get scoped() {
-    return new FileScope(this.repository.createQueryBuilder('File'));
+export class SubscriptionPlanRepository extends BaseRepository<
+  File,
+  Prisma.FileCreateInput,
+  Prisma.FileUpdateInput,
+  Prisma.FileWhereUniqueInput,
+  Prisma.FileWhereInput,
+  PrismaService['file'],
+  FileScope
+> {
+  constructor(protected readonly prisma: PrismaService) {
+    super(prisma, prisma.file, new FileScope(prisma));
   }
 }
