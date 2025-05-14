239c9af5f274df9d34189d4d4256b8374b797325
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-14 23:15:30 +0100

    Refactor file and folder action menu hooks and components

--- libs/core/web/react/src/lib/hooks/index.ts
+++ libs/core/web/react/src/lib/hooks/index.ts
@@ -4,7 +4,7 @@ export * from './use-mobile';
 export * from './auth.hook';
 export * from './use-file-table.hook';
 export * from './use-context-menu.hook';
-export * from './use-file-action-menu.hook';
+export * from './use-file-action-menu';
 
 export * from './storage.hook';
 export * from './folder.hook';
---
+++ libs/core/web/react/src/lib/hooks/use-file-action-menu/use-file-menu.tsx
@@ -0,0 +1,117 @@
+import {
+  Info,
+  ExternalLink,
+  Share2,
+  Link,
+  UserCog,
+  Download,
+  Copy,
+  Move,
+  Trash2,
+  Star,
+  Pin,
+  Eye,
+  Activity,
+  TextCursorInput as RenameIcon,
+  Eye as PreviewIcon,
+} from 'lucide-react';
+import { MenuItem } from '@keepcloud/web-core/react';
+import { iconClassName, itemClassName } from './config';
+
+export const useFileMenuItems = (file: {
+  id: string;
+  name: string;
+}): MenuItem[] => [
+  {
+    label: 'Preview',
+    icon: <PreviewIcon className={iconClassName} />,
+    onClick: () => console.log(`Preview ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Info',
+    icon: <Info className={iconClassName} />,
+    onClick: () => console.log(`View info for ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Open in',
+    icon: <ExternalLink className={iconClassName} />,
+    onClick: () => console.log(`Open ${file.name} in...`),
+    className: itemClassName,
+  },
+  {
+    label: 'Share',
+    icon: <Share2 className={iconClassName} />,
+    onClick: () => console.log(`Share ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Copy link',
+    icon: <Link className={iconClassName} />,
+    onClick: () => console.log(`Copy link for ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Manage permissions',
+    icon: <UserCog className={iconClassName} />,
+    onClick: () => console.log(`Manage permissions for ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Download',
+    icon: <Download className={iconClassName} />,
+    onClick: () => console.log(`Download ${file.name}`),
+    className: itemClassName,
+    separatorAfter: true,
+  },
+  {
+    label: 'Rename',
+    icon: <RenameIcon className={iconClassName} />,
+    onClick: () => console.log(`Rename ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Copy',
+    icon: <Copy className={iconClassName} />,
+    onClick: () => console.log(`Copy ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Move',
+    icon: <Move className={iconClassName} />,
+    onClick: () => console.log(`Move ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Move to Trash',
+    icon: <Trash2 className={iconClassName} />,
+    onClick: () => console.log(`Move ${file.name} to Trash`),
+    className: itemClassName,
+    separatorAfter: true,
+  },
+  {
+    label: 'Star',
+    icon: <Star className={iconClassName} />,
+    onClick: () => console.log(`Star ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Pin to KeepCloud',
+    icon: <Pin className={iconClassName} />,
+    onClick: () => console.log(`Pin ${file.name} to KeepCloud`),
+    className: itemClassName,
+  },
+  {
+    label: 'Show pinned items',
+    icon: <Eye className={iconClassName} />,
+    onClick: () => console.log(`Show pinned items for ${file.name}`),
+    className: itemClassName,
+  },
+  {
+    label: 'Activity',
+    icon: <Activity className={iconClassName} />,
+    onClick: () => console.log(`View activity for ${file.name}`),
+    className: itemClassName,
+  },
+];
