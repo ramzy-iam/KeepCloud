203ca4caa53f916857cc9ad83e548830a4b956bf
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-14 20:49:59 +0100

    Remove "Shared with me" heading from SharedWithMeComponent

--- apps/web/app/routes/app/home/shared-with-me.tsx
+++ apps/web/app/routes/app/home/shared-with-me.tsx
@@ -6,9 +6,6 @@ export default function SharedWithMeComponent() {
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col gap-3">
-        <h3 className="sticky -top-[1px] z-[1] bg-background p-1.5 text-18-medium text-heading">
-          Shared with me
-        </h3>
         <FolderView
           items={[]}
           title={SYSTEM_FILE.SHARED_WITH_ME.name}
