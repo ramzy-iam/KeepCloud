87a4620c6ec507a1bebd7173a16dba7ba06f0a0a
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-19 22:46:39 +0100

    feat:add support location breadcrumb for mobile using popover

--- apps/web/app/components/files/folder-breadcrumb.tsx
+++ apps/web/app/components/files/folder-breadcrumb.tsx
@@ -12,22 +12,12 @@ import {
   BreadcrumbEllipsis,
   useSidebar,
   TooltipProviderWrapper,
-  HoverCardTrigger,
-  HoverCard,
-  HoverCardContent,
-  ROUTE_PATH,
 } from '@keepcloud/web-core/react';
 import { useNavigate } from 'react-router';
 import { FileAncestor, FileMinViewDto } from '@keepcloud/commons/dtos';
 
 interface FolderBreadcrumbProps {
   folder: FileMinViewDto;
-  onBreadcrumbClick?: (ancestor: FileAncestor) => void;
-}
-
-interface FileLocationBreadcrumbHoverProps {
-  folder: FileMinViewDto;
-  className?: string;
   onBreadcrumbClick?: (ancestor: FileAncestor) => void;
 }
 
@@ -74,7 +64,8 @@ export const FolderBreadcrumb = ({
                       key={ancestor.id}
                       className="cursor-pointer"
                       asChild
-                      onClick={() => {
+                      onClick={(e) => {
+                        e.stopPropagation();
                         onBreadcrumbClick && onBreadcrumbClick(ancestor);
                       }}
                     >
@@ -94,7 +85,8 @@ export const FolderBreadcrumb = ({
             <TooltipProviderWrapper content={ancestor.name}>
               <BreadcrumbItem
                 className="cursor-pointer rounded-[16px] px-4 py-1 text-heading hover:bg-stroke-200 dark:hover:bg-white/5"
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   onBreadcrumbClick && onBreadcrumbClick(ancestor);
                 }}
               >
@@ -117,75 +109,5 @@ export const FolderBreadcrumb = ({
         </TooltipProviderWrapper>
       </BreadcrumbList>
     </Breadcrumb>
-  );
-};
-
-const MiniBreadcrumb = ({
-  folder,
-  onBreadcrumbClick,
-}: FileLocationBreadcrumbHoverProps) => {
-  const ancestors = folder.ancestors ?? [];
-  const navigate = useNavigate();
-  const defaultOnClick = (ancestor: FileAncestor) => {
-    const route = ancestor.isSystem
-      ? ROUTE_PATH.system(ancestor.code)
-      : ROUTE_PATH.folderDetails(ancestor.id);
-
-    navigate(route);
-  };
-
-  return (
-    <Breadcrumb className="px-2 py-1 text-sm text-secondary-foreground">
-      <BreadcrumbList>
-        {ancestors.map((ancestor) => (
-          <React.Fragment key={ancestor.id}>
-            <BreadcrumbItem
-              onClick={() => {
-                onBreadcrumbClick
-                  ? onBreadcrumbClick(ancestor)
-                  : defaultOnClick(ancestor);
-              }}
-              className="cursor-pointer rounded-[16px] px-4 py-2 text-secondary-foreground hover:bg-stroke-200 dark:hover:bg-white/5"
-            >
-              {ancestor.name}
-            </BreadcrumbItem>
-            {ancestors.length > 1 && <BreadcrumbSeparator />}
-          </React.Fragment>
-        ))}
-      </BreadcrumbList>
-    </Breadcrumb>
-  );
-};
-
-export const FileLocationBreadcrumb = ({
-  folder,
-  className,
-}: {
-  folder: FileMinViewDto;
-  className?: string;
-}) => {
-  const ancestors = folder?.ancestors ?? [];
-
-  if (!folder || ancestors.length === 0) return null;
-
-  const nearestAncestor = ancestors[ancestors.length - 1];
-
-  return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <div
-          className={`w-full max-w-[130px] cursor-pointer truncate overflow-hidden text-right whitespace-nowrap text-secondary-foreground ${className}`}
-        >
-          {nearestAncestor.name}
-        </div>
-      </HoverCardTrigger>
-      <HoverCardContent
-        side="left"
-        sideOffset={-200}
-        className="w-fit max-w-svw rounded-[32px] p-2"
-      >
-        <MiniBreadcrumb folder={folder} />
-      </HoverCardContent>
-    </HoverCard>
   );
 };
