8bd35678f4ffe3877b7fc878f4f25f24e061ec86
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-19 21:47:58 +0100

    feat:add system folder from the api

--- libs/core/web/react/src/lib/constants/route.constant.ts
+++ libs/core/web/react/src/lib/constants/route.constant.ts
@@ -1,3 +1,5 @@
+import { SYSTEM_FILE } from '@keepcloud/commons/constants';
+
 export const ROUTE_PATH = {
   home: '/home',
   login: '/auth',
@@ -8,5 +10,18 @@ export const ROUTE_PATH = {
   trash: '/home/trash',
   folderDetails(folderId: string) {
     return `/folders/${folderId}`;
+  },
+
+  system(code?: string) {
+    switch (code) {
+      case SYSTEM_FILE.MY_STORAGE.code:
+        return this.folder;
+      case SYSTEM_FILE.SHARED_WITH_ME.code:
+        return this.sharedWithMe;
+      case SYSTEM_FILE.TRASH.code:
+        return this.trash;
+      default:
+        return '/';
+    }
   },
 };
