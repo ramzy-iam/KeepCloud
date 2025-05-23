09ed902623accea7ab59f967e9c6350f80ac7b6c
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-05-23 15:11:44 +0100

    remove rls config

--- libs/core/db/src/lib/prisma/prisma.service.ts
+++ libs/core/db/src/lib/prisma/prisma.service.ts
@@ -1,34 +1,11 @@
-import {
-  Injectable,
-  OnModuleDestroy,
-  OnModuleInit,
-  Scope,
-  Inject,
-} from '@nestjs/common';
-import { REQUEST } from '@nestjs/core';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { bypassRLS, forUser } from './extensions';
-import { Request } from 'express';
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor(
-    @Inject(REQUEST)
-    private readonly request: Request & { user?: { sub: string } },
-  ) {
-    const userId = request?.user?.sub ?? 'anonymous'; //TODO: inject the userId from the request
-
-    const extendedClient = new PrismaClient()
-      .$extends(bypassRLS())
-      .$extends(forUser(userId));
-
-    super();
-    Object.assign(this, extendedClient);
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
