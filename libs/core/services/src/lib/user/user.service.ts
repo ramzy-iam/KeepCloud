6287f4f4c4b929ac5c685d4e8677f173d8d9ef66
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-09-12 23:57:52 +0100

    feat: implement user filtering and searching functionality in UserService and UserController

--- libs/core/services/src/lib/user/user.service.ts
+++ libs/core/services/src/lib/user/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { SubscriptionPlanRepository, UserRepository } from '@keepcloud/core/db';
+import {
+  SubscriptionPlanRepository,
+  User,
+  UserRepository,
+} from '@keepcloud/core/db';
 import { TokenPayload } from 'google-auth-library';
-import { User } from '@prisma/client';
 import { UserNotFoundException } from '@keepcloud/commons/backend';
 import { SYSTEM_FILE } from '@keepcloud/commons/constants';
+import { UserFilterDto, PaginationDto } from '@keepcloud/commons/dtos';
 
 @Injectable()
 export class UserService {
@@ -136,5 +140,17 @@ export class UserService {
         storageUsed: actualStorageUsed,
       },
     );
+  }
+
+  async findAll(
+    filters: UserFilterDto,
+    currentUserId: string,
+  ): Promise<PaginationDto<User>> {
+    const { query, page, pageSize } = filters;
+
+    return this.userRepository.scoped
+      .searchByNameOrEmail(query)
+      .filterNotById(currentUserId)
+      .getManyPaginated(page, pageSize);
   }
 }
