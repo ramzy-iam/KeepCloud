5c284b4a61d649456d77345d572911facf749df1
Ramsès Feukeng <ramsesfeukeng@gmail.com>
2025-09-22 21:06:12 +0100

    feat: refactor file permission roles and remove unused sharing hooks

--- libs/commons/src/lib/dtos/file-sharing.dto.ts
+++ libs/commons/src/lib/dtos/file-sharing.dto.ts
@@ -8,7 +8,7 @@ import {
 
 import { ErrorCode } from '../constants';
 import { Expose, Type } from 'class-transformer';
-import { FilePermissionRole } from '@prisma/client';
+import { FilePermissionRole } from '../types/file-permissions';
 import { UserProfileDto } from './user.dto';
 import { FileMinViewDto } from './file.dto';
 
